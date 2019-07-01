1d7fbe82579cedab83d5c0ea345a0707a068893f
miguelgzpro <52212705+miguelgzpro@users.noreply.github.com>
2019-06-30 23:17:44 -0300

    Create prueba.js

---
+++ prueba.js
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    <script>
+     /*
+    //Declarar tres objetos de nombre Hugo, Paco y Luis. 
+      cada obtejo debe tener estos atributos:  nombre, edad, ciudad.   
+      Usted debe asignar edades y ciudades a cada objeto
+     */
+
+
+
+        var nombre = Hugo;
+        var edad = 18;
+        var ciudad = quito;
+
+
+        var nombre = Paco;
+        var edad = 20;
+        var ciudad = guayaquil;
+
+
+        var nombre = Luis;
+        var edad = 27;
+        var ciudad = ambato;
+
+        // 1
+        var amigos = [];
+
+        //2
+        amigos.push = ("Hugo")
+        amigos.push = ("Paco")
+        amigos.push = ("Luis")
+
+        //3
+        amigos.length
+
+        //4
+        var Elena = new object();
+        var Elena = edad("18");
+        var Elena = ciudad("cuenca");
+
+        //5
+        var numeroAmigos = [];
+        numeroAmigos.push("hugo")
+        numeroAmigos.push("paco")
+        numeroAmigos.push("luis")
+        numeroAmigos.push("elena")
+
+        return numeroAmigos
+        console.log(numeroAmigos.length);
+
+        //6
+        //Calcular si es par o impar y imprime el mensaje
+        var saludo = function(Object) {
+            {
+                if (num % 2 == 0) {
+                    console.log("“Saluton kaj bonvenon”");
+                } else {
+                    console.log("Hello moto");
+                }
+            }
+        }
+
+
+        //7
+        var arriba = function(Obj) {
+            if (Obj > numeroMaximo) {
+                console.log("fin");
+            } else {
+                console.log("Obj");
+                return numeroMaximo("Obj = " + 1);
+            }
+        }
+        var repetidor = function(contador) {
+
+            if (contador > numeroMaximo) {
+                console.log("fin");
+                return 0;
+            } else {
+                //logica
+                console.log("contador = " + contador);
+                return repetidor(contador + 1);
+            }
+        }
+
+
+        //8
+        var abajo = function(obj) {
+            if (numero === 0) {
+                return 0;
+            } else {
+                console.log(num)
+                return abajo(numero - 1);
+
+
+
+            }
+        }
+
+
+        //9
+
+        var edadTotal = function(Object) {
+            if (object === edad) {} else {
+                suma = (object, object, object, object)
+                alert = ("suma de las 4 edades = " + suma);
+            }
+        }
+    </script>
+</body>
+
+</html>
